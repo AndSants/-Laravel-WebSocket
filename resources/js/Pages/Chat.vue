316e42e10b5fdc9c8a182b5a1984301088fb0889
André Santos <andresantos.iron@gmail.com>
2022-04-12 00:07:22 -0300

    created component Vue (chat) , marking HTML and stylization

--- resources/js/Pages/Chat.vue
+++ resources/js/Pages/Chat.vue
@@ -5,11 +5,119 @@
                 Chat
             </h2>
         </template>
-
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <h1>Componente Chat</h1>
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex" style="min-height: 400px; max-height: 400px;">
+                    <!-- List users -->
+                    <div class="w-3/12 bg-gray-200 bg-opacity-25 border-r border-gray-200 overflow-y-scroll">
+                        <ul>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    João Pedro
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Lucas Carlos
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <!-- example people -->
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Teste 1
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Teste 2
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Teste 3
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Teste 4
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Teste 5
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <!-- end example people -->
+                        </ul>
+                    </div>
+                    <!-- Box message -->
+                    <div class="w-9/12 flex flex-col justify-between">
+                        <!-- message -->
+                        <div class="w-full p-6 flex flex-col overflow-y-scroll">
+                            <div class="w-full mb-3 text-right">
+                                <p class="inline-block p-2 rounded-md messageFromMe bg-indigo-300 bg-opacity-25" style="max-width: 75%;">
+                                    Olá!
+                                </p>
+                                <span class="block mt-1 text-gray-500 text-xs">11/04/2022 23:12</span>
+                            </div>
+
+                            <div class="w-full mb-3 ">
+                                <p class="inline-block p-2 rounded-md messageToMe bg-gray-300 bg-opacity-25" style="max-width: 75%;">
+                                    Oi!
+                                </p>
+                                <span class="block mt-1 text-gray-500 text-xs">11/04/2022 23:13</span>
+                            </div>
+                            <!-- example message -->
+                            <div class="w-full mb-3 ">
+                                <p class="inline-block p-2 rounded-md messageToMe bg-gray-300 bg-opacity-25" style="max-width: 75%;">
+                                    Oi!
+                                </p>
+                                <span class="block mt-1 text-gray-500 text-xs">11/04/2022 23:13</span>
+                            </div>
+                            <div class="w-full mb-3 ">
+                                <p class="inline-block p-2 rounded-md messageToMe bg-gray-300 bg-opacity-25" style="max-width: 75%;">
+                                    Oi!
+                                </p>
+                                <span class="block mt-1 text-gray-500 text-xs">11/04/2022 23:13</span>
+                            </div>
+                            <div class="w-full mb-3 ">
+                                <p class="inline-block p-2 rounded-md messageToMe bg-gray-300 bg-opacity-25" style="max-width: 75%;">
+                                    Oi!
+                                </p>
+                                <span class="block mt-1 text-gray-500 text-xs">11/04/2022 23:13</span>
+                            </div>
+                            <div class="w-full mb-3 ">
+                                <p class="inline-block p-2 rounded-md messageToMe bg-gray-300 bg-opacity-25" style="max-width: 75%;">
+                                    Oi!
+                                </p>
+                                <span class="block mt-1 text-gray-500 text-xs">11/04/2022 23:13</span>
+                            </div>
+                            <div class="w-full mb-3 ">
+                                <p class="inline-block p-2 rounded-md messageToMe bg-gray-300 bg-opacity-25" style="max-width: 75%;">
+                                    Oi!
+                                </p>
+                                <span class="block mt-1 text-gray-500 text-xs">11/04/2022 23:13</span>
+                            </div>
+                            <!-- end example message -->
+                        </div>
+                        <!-- form -->
+                        <div class="w-full bg-gray-200 bg-opacity-25 p-6 border-t border-gray-200">
+                            <form action="">
+                                <div class="flex rounded-md overflow-hidden border border-gray-300">
+                                    <input class="flex-1 px-4 py-2 text-sm focus:outline-none" type="text" name="" id="">
+                                    <button type="submit" class="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2">Enviar</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
